a0d17b84d41deafce5cc5005857e9bf0d256488b
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-09-13 21:53:37 +0530

    frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,9 @@
-// index.js
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const path = require("path");
-const { connection } = require("./config/db"); // Assuming your connection configuration is in "config/db.js"
+const { connection } = require("./config/db");
 
 // Import environment variables
 require("dotenv").config();
@@ -14,9 +13,17 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Create the "uploads" folder if it doesn't exist
+const fs = require("fs");
+const uploadsFolder = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsFolder)) {
+  fs.mkdirSync(uploadsFolder);
+}
 
 const storage = multer.diskStorage({
-  destination: "./uploads/",
+  destination: "uploads/",
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
   }
@@ -26,7 +33,7 @@ const upload = multer({ storage });
 
 const Product = mongoose.model("Product", {
   name: String,
-  imageUrl: String,
+  imageUrl: String, // Store the relative path to the image
   description: String,
   price: Number
 });
@@ -34,13 +41,13 @@ const Product = mongoose.model("Product", {
 // Define routes
 app.post("/upload", upload.single("image"), async (req, res) => {
   try {
-    // if (!req.file) {
-    //   return res.status(400).json({ error: "No file uploaded" });
-    // }
-
-    // const imageUrl = req.file.filename;
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
     const { name, description, price } = req.body;
-    const product = new Product({ name, description, price });
+    const imageUrl = `/uploads/${req.file.filename}`;
+
+    const product = new Product({ name, description, price, imageUrl });
     await product.save();
     res.status(201).json(product);
   } catch (error) {
@@ -49,10 +56,57 @@ app.post("/upload", upload.single("image"), async (req, res) => {
   }
 });
 
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+  if (email === "aajad@gmail.com" && password === "aajad") {
+    res.status(200).json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ error: "Invalid email or password" });
+  }
+});
+
 app.get("/products", async (req, res) => {
   try {
     const products = await Product.find();
+    console.log(products);
     res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.put("/products/:productId", upload.single("image"), async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const { name, description, price } = req.body;
+    const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      { name, description, price, imageUrl },
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.delete("/products/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const deletedProduct = await Product.findByIdAndRemove(productId);
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.status(204).json();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Something went wrong" });
---
+++ frontend/src/component/ProductList.jsx
@@ -0,0 +1,152 @@
+//ProductList.jsx
+import React, { useState } from "react";
+import axios from "axios";
+import EditProductModal from "./EditProductModal";
+
+const ProductList = ({ products, refreshProductList }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [editedProduct, setEditedProduct] = useState({
+    _id: "",
+    name: "",
+    description: "",
+    price: "",
+    image: null,
+  });
+
+  const handleEdit = (product) => {
+    setEditMode(true);
+    // Populate the form with the product data when in edit mode
+    setEditedProduct({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      image: product.imageUrl, // Use the current product's image URL
+    });
+  };
+
+  const handleCancelEdit = () => {
+    setEditMode(false);
+    setEditedProduct({
+      _id: "",
+      name: "",
+      description: "",
+      price: "",
+      image: null,
+    });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedProduct({ ...editedProduct, [name]: value });
+  };
+
+  const handleImageChange = (e) => {
+    const imageFile = e.target.files[0];
+    setEditedProduct({ ...editedProduct, image: imageFile });
+  };
+
+  const handleUpdateProduct = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("name", editedProduct.name);
+      formData.append("description", editedProduct.description);
+      formData.append("price", editedProduct.price);
+      if (editedProduct.image) {
+        formData.append("image", editedProduct.image);
+      }
+
+      // Replace with your API endpoint
+      await axios.put(
+        `http://localhost:8080/products/${editedProduct._id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      setEditMode(false);
+      setEditedProduct({
+        _id: "",
+        name: "",
+        description: "",
+        price: "",
+        image: null,
+      });
+      refreshProductList(); // Refresh the product list after update
+    } catch (error) {
+      console.error("Error updating product:", error);
+    }
+  };
+
+  const handleDeleteProduct = async (productId) => {
+    try {
+      // Replace with your API endpoint
+      await axios.delete(`http://localhost:8080/products/${productId}`);
+      refreshProductList(); // Refresh the product list after delete
+    } catch (error) {
+      console.error("Error deleting product:", error);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {products.map((product) => (
+        <div
+          key={product._id}
+          className="border rounded-lg overflow-hidden hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105 p-4 bg-gray-100"
+        >
+          {/* Image */}
+          <div className="mb-4">
+            <img
+              src={`http://localhost:8080${product.imageUrl}`}
+              alt={product.name}
+              className="w-full h-64 object-cover rounded-lg"
+            />
+          </div>
+
+          {/* Product Info */}
+          <div className="p-4">
+            <h3 className="text-2xl font-semibold text-blue-800 mb-2">
+              {product.name}
+            </h3>
+            <p className="text-gray-700">{product.description}</p>
+            <p className="text-green-600 text-xl mt-2">${product.price}</p>
+          </div>
+
+          {/* Edit/Delete Buttons */}
+          <div className="flex justify-between p-4">
+            <div className="space-x-2">
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                onClick={() => handleEdit(product)}
+              >
+                Edit
+              </button>
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+                onClick={() => handleDeleteProduct(product._id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+      {/* Conditionally render the edit modal */}
+      {editMode && (
+        <EditProductModal
+          editedProduct={editedProduct}
+          handleInputChange={handleInputChange}
+          handleImageChange={handleImageChange}
+          handleUpdateProduct={handleUpdateProduct}
+          handleCancelEdit={handleCancelEdit}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
